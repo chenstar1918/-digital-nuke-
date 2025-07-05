f91dd2675cb32d81743522bb3f9ed81b2bdfb506
chenstar1918 <chenstar1215@163.com>
2025-07-05 19:51:27 +0800

    Delete nuke10.py

--- nuke10.py
+++
@@ -1,9 +0,0 @@
-def nuke(n):
-    a=[];
-    for i in range(10):
-        if(n>1):
-            a.append(nuke(n-1));
-        else:
-            a.append(i);
-    return a;
-print(nuke(int(input("输入数字(千万不要超过10)"))));
